d9d74564b0da0ede459cca615a758f48cac48028
anniemcfarland7 <anniemcfarland@aol.com>
2021-02-01 15:28:49 -0600

    updating files

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-
+const path = require('path')
+const fs = require('fs')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-// Displays a single character, or returns false
-app.get('/api/notes/:id', (req, res) => {
-  const chosen = req.params.id;
+app.get('/api/notes', (req, res) => res.sendFile(path.json(__dirname, "./db/db.json")));
 
-  console.log(chosen)
 
-})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on: http://localhost:${PORT}`);
