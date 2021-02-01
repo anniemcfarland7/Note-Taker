6293f935531479f874f7c742fac85ace03103ae7
anniemcfarland7 <anniemcfarland@aol.com>
2021-02-01 15:39:34 -0600

    updating files

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const fs = require('fs')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 
-// Basic route that sends the user first to the AJAX Page
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
